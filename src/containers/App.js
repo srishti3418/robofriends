a89a523d13b8780f5f0191ee66e00a6d9e5fe91e
srishti3418 <76532197+srishti3418@users.noreply.github.com>
2021-06-07 14:27:19 +0530

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends Component{
 	} 
 
 	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users')
+		fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
 		.then(response=> response.json())
 		.then(users=>this.setState({robots:users}));
 	}
